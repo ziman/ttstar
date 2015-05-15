cf12e125cbf969fd5c344b90b6290f709284e11e
Matus Tejiscak <ziman@functor.sk>
2015-05-15 17:33:15 +0100

    Sort-of working solution.

--- Main.hs
+++ Main.hs
@@ -14,32 +14,39 @@ infixr 3 ~>
 (~>) = Bind Pi Nothing "_"
 
 infixr 3 .->
-(.->) :: String -> TT -> TT
-n .-> tm = Bind Lam Nothing n (C TInt) tm
+(.->) :: (String, TT) -> TT -> TT
+(n, ty) .-> tm = Bind Lam Nothing n ty tm
 
 infixl 4 !
 (!) :: TT -> TT -> TT
 (!) = App Nothing
 
+int :: TT
+int = C TInt
+
 testTerm :: TT
-testTerm = "x" .-> V "x"
+testTerm = ("x", int) .-> V "x"
 
 testProg :: Program (Maybe Relevance)
 testProg = Prog
     [ Def Nothing "id" intFun
-        $ Fun ("y" .-> V "y")
-    {-
+        $ Fun (("y", int) .-> V "y")
     , Def Nothing "const_42" intFun
-        $ Fun ("x" .-> C (Int 42))
-    , Def Nothing "f" (intFun ~> C TInt ~> intFun ~> C TInt ~> C TInt)
-        $ Fun ("g" .-> "z" .-> "h" .-> "w" .-> Prim Plus ! (V "g" ! V "z") ! (V "h" ! V "w"))
-    -}
-    , Def (Just R) "main" (C TInt)
-    --    $ Fun (V "f" ! V "id" ! C (Int 3) ! V "const_42" ! C (Int 7))
-        $ Fun (V "id" ! C (Int 42))
+        $ Fun (("x", int) .-> C (Int 42))
+    , Def Nothing "f" (intFun ~> int ~> intFun ~> int ~> int)
+        $ Fun (
+            ("g", intFun) .->
+              ("z", int) .->
+                ("h", intFun) .->
+                  ("w", int) .->
+                    Prim Plus ! (V "g" ! V "z") ! (V "h" ! V "w")
+          )
+    , Def (Just R) "main" (int)
+        $ Fun (V "f" ! V "id" ! C (Int 3) ! V "const_42" ! C (Int 7))
+    --    $ Fun (V "id" ! C (Int 42))
     ]
   where
-    intFun = C TInt ~> C TInt
+    intFun = int ~> int
 
 main :: IO ()
 main = do
