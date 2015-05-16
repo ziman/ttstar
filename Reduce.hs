162041689b336181b756211dca89c07d6e921997
Matus Tejiscak <ziman@functor.sk>
2015-05-16 01:33:05 +0100

    Implement reduction.

--- Reduce.hs
+++ Reduce.hs
@@ -5,8 +5,50 @@ import qualified Data.Map as M
 
 type Ctx' r = M.Map Name (r, TT' r, Maybe (TT' r))
 
-reduce :: Ctx' r -> TT' r -> TT' r
-reduce ctx tm = tm
+reduce :: ShowR r => Ctx' r -> TT' r -> TT' r
+reduce ctx t@(V n)
+    | Just (r, ty, mtm) <- M.lookup n ctx
+    = case mtm of
+        Nothing -> t
+        Just tm -> reduce ctx tm
+
+    | otherwise = error $ "unknown variable " ++ show n
+
+reduce ctx (Bind b r n ty tm) = Bind b r n (reduce ctx ty) (reduce ctx tm)
+reduce ctx (App r f x)
+    | Bind Lam r' n' ty' tm' <- redF
+    = reduce ctx $ subst n' x tm'
+
+    | otherwise = App r redF (reduce ctx x)
+  where
+    redF = reduce ctx f
+
+reduce ctx t@(Prim op) = t
+
+reduce ctx (Case s alts) = redCase ctx (reduce ctx s) alts
+
+reduce ctx t@(C c)     = t
+reduce ctx t@Erased    = t
+
+redCase :: ShowR r => Ctx' r -> TT' r -> [Alt r] -> TT' r
+redCase ctx _ (DefaultCase tm : _) = reduce ctx tm
+redCase ctx (C c) (ConstCase c' tm : as) | c == c' = reduce ctx tm
+redCase ctx s (ConCase cn _r ns tm : as)
+    | (V cn', args) <- unApply s
+    , length args == length ns
+    = reduce ctx $ subst' (zip ns args) tm
+  where
+    subst' ((n,x):xs) tm = subst' xs $ subst n x tm
+    subst' [] tm = tm
+
+redCase ctx s (_ : as) = redCase ctx s as
+redCase ctx s [] = error $ "uncovered case: " ++ show s
+
+unApply :: TT' r -> (TT' r, [TT' r])
+unApply tm = ua tm []
+  where
+    ua (App _ f x) args = ua f (x : args)
+    ua tm args = (tm, args)
 
 subst :: Name -> TT' r -> TT' r -> TT' r
 subst n tm t@(V n')
