6e99b58567dae3e606546300138e8288dff840d1
Matus Tejiscak <ziman@functor.sk>
2016-05-11 00:58:11 +0100

    Remove eds from Prune.

--- Erasure/Prune.hs
+++ Erasure/Prune.hs
@@ -6,46 +6,43 @@ import Control.Applicative
 import qualified Data.Set as S
 
 prune :: Program Relevance VoidConstrs -> Program () VoidConstrs
-prune (Prog defs) = Prog $ pruneDefs eds defs
-  where
-    -- elided defs
-    eds = S.fromList [n | Def n E _ _ _ <- defs]
+prune (Prog defs) = Prog $ pruneDefs defs
 
-pruneDef :: S.Set Name -> Def Relevance VoidConstrs -> [Def () VoidConstrs]
-pruneDef eds (Def n E ty body mcs) = []
-pruneDef eds (Def n R ty body mcs) = [Def n () (V Blank) (pruneBody eds body) Nothing]
+pruneDef :: Def Relevance VoidConstrs -> [Def () VoidConstrs]
+pruneDef (Def n E ty body mcs) = []
+pruneDef (Def n R ty body mcs) = [Def n () (V Blank) (pruneBody body) Nothing]
 
-pruneBody :: S.Set Name -> Body Relevance -> Body ()
-pruneBody eds (Abstract a)  = Abstract a
-pruneBody eds (Term tm)     = Term $ pruneTm eds tm
-pruneBody eds (Patterns cf) = Patterns $ pruneCaseFun eds cf
+pruneBody :: Body Relevance -> Body ()
+pruneBody (Abstract a)  = Abstract a
+pruneBody (Term tm)     = Term $ pruneTm tm
+pruneBody (Patterns cf) = Patterns $ pruneCaseFun cf
 
-pruneCaseFun :: S.Set Name -> CaseFun Relevance -> CaseFun ()
-pruneCaseFun eds (CaseFun args ct)
-    = CaseFun (pruneDefs eds args) (pruneCaseTree eds ct)
+pruneCaseFun :: CaseFun Relevance -> CaseFun ()
+pruneCaseFun (CaseFun args ct)
+    = CaseFun (pruneDefs args) (pruneCaseTree ct)
 
-pruneCaseTree :: S.Set Name -> CaseTree Relevance -> CaseTree ()
-pruneCaseTree eds (Leaf tm) = Leaf $ pruneTm eds tm
-pruneCaseTree eds (Case E s [Alt lhs rhs]) = pruneCaseTree eds rhs
-pruneCaseTree eds (Case R s alts) = Case () (pruneTm eds s) $ map (pruneAlt eds) alts
+pruneCaseTree :: CaseTree Relevance -> CaseTree ()
+pruneCaseTree (Leaf tm) = Leaf $ pruneTm tm
+pruneCaseTree (Case E s [Alt lhs rhs]) = pruneCaseTree rhs
+pruneCaseTree (Case R s alts) = Case () (pruneTm s) $ map pruneAlt alts
 
-pruneAlt :: S.Set Name -> Alt Relevance -> Alt ()
-pruneAlt eds (Alt lhs rhs) = Alt (pruneAltLHS eds lhs) (pruneCaseTree eds rhs)
+pruneAlt :: Alt Relevance -> Alt ()
+pruneAlt (Alt lhs rhs) = Alt (pruneAltLHS lhs) (pruneCaseTree rhs)
 
-pruneAltLHS :: S.Set Name -> AltLHS Relevance -> AltLHS ()
-pruneAltLHS eds Wildcard = Wildcard
-pruneAltLHS eds (Ctor cn args eqs)
-    = Ctor cn (pruneDefs eds args) [(n, pruneTm eds tm) | (n, tm) <- eqs]
+pruneAltLHS :: AltLHS Relevance -> AltLHS ()
+pruneAltLHS Wildcard = Wildcard
+pruneAltLHS (Ctor cn args eqs)
+    = Ctor cn (pruneDefs args) [(n, pruneTm tm) | (n, tm) <- eqs]
 
-pruneDefs :: S.Set Name -> [Def Relevance VoidConstrs] -> [Def () VoidConstrs]
-pruneDefs eds = concatMap (pruneDef eds)
+pruneDefs :: [Def Relevance VoidConstrs] -> [Def () VoidConstrs]
+pruneDefs = concatMap pruneDef
 
-pruneTm :: S.Set Name -> TT Relevance -> TT ()
-pruneTm eds (V n) = V n
-pruneTm eds (I n ty) = error $ "non-specialised instance found in pruneTm: " ++ show (n, ty)
-pruneTm eds (Bind b d tm)
-    = case pruneDef eds d of
-        []   -> pruneTm (S.insert (defName d) eds) tm  -- remember the elided definition
-        [d'] -> Bind b d' (pruneTm eds tm)
-pruneTm eds (App E f x) = pruneTm eds f
-pruneTm eds (App R f x) = App () (pruneTm eds f) (pruneTm eds x)
+pruneTm :: TT Relevance -> TT ()
+pruneTm (V n) = V n
+pruneTm (I n ty) = error $ "non-specialised instance found in pruneTm: " ++ show (n, ty)
+pruneTm (Bind b d tm)
+    = case pruneDef d of
+        []   -> pruneTm tm
+        [d'] -> Bind b d' (pruneTm tm)
+pruneTm (App E f x) = pruneTm f
+pruneTm (App R f x) = App () (pruneTm f) (pruneTm x)
