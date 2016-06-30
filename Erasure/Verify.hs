e735c42c7958dd1f6aee11e55e5b31e299a22c29
Matus Tejiscak <ziman@functor.sk>
2016-06-30 15:07:16 +0100

    Start implementing Verify.hs.

--- Erasure/Verify.hs
+++ Erasure/Verify.hs
@@ -9,25 +9,40 @@ import Erasure.Check
 import Erasure.Meta
 import Erasure.Solve
 
+import Data.Traversable
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Lens.Family2
+import Control.Monad
+import Control.Applicative
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
 
 data VerError
     = TCFailure TCFailure
     | InconsistentAnnotation
+    | NotImplemented
     deriving Show
 
--- TODO: make this a separate/independent typechecker
--- rather than reusing the possibly buggy typechecker-elaborator?
 
-verify :: Program Relevance -> Either VerError ()
-verify prog =
-    case check (prog & progRelevance %~ Fixed) of
-        Left tcfail -> Left (TCFailure tcfail)
-        Right ctx ->
-            let (uses, residue) = solve . defConstraints $ ctx M.! (UN "main")
-              in
-                if Fixed E `S.member` uses
-                  then Left InconsistentAnnotation
-                  else Right ()
+data VerFailure = VerFailure VerError [String] deriving Show
+type Ver a = ReaderT (VerTraceback, Ctx Relevance) (Except VerFailure) a
+type VerTraceback = [String]
+
+runVer :: Ctx Relevance -> Ver a -> Either VerFailure a
+runVer ctx ver = runExcept $ runReaderT ver ([], ctx)
+
+verFail :: VerError -> Ver a
+verFail e = do
+    (tb, ctx) <- ask
+    lift . throwE $ VerFailure e tb
+
+verify :: Program Relevance -> Either VerFailure ()
+verify prog = runVer (builtins E) $ verProg prog
+
+verProg :: Program Relevance -> Ver ()
+verProg (Prog defs) = mapM_ verDef defs
+
+verDef :: Def Relevance -> Ver ()
+verDef (Def n r ty b cs) = verFail NotImplemented
