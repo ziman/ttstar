bbe7147a8efc08413644d7afd1896df8bb85509c
Matus Tejiscak <ziman@functor.sk>
2016-09-23 12:16:26 +0100

    Improve layout.

--- Codegen/Scheme.hs
+++ Codegen/Scheme.hs
@@ -5,15 +5,16 @@ import Pretty ()
 import Util.PrettyPrint
 import Codegen.Common
 
+indent :: Doc -> Doc
+indent = nest 2
+
 cgName :: Name -> Doc
 cgName = text . show
 
 cgTm :: TT () -> Doc
 cgTm (V n) = cgName n
 cgTm tm@(I n ty) = error $ "e-instance in codegen: " ++ show tm
-cgTm (Bind Lam (Def n () ty (Abstract Var) cs) rhs) = parens (
-        text "lambda" <+> parens (cgName n) <+> cgTm rhs
-    )
+cgTm (Bind Lam (Def n () ty (Abstract Var) cs) rhs) = cgLambda n $ cgTm rhs
 cgTm (Bind Let (Def n () ty (Term tm) cs) rhs) = parens (
         text "let" <+> parens (parens (
             cgName n <+> cgTm tm
@@ -24,26 +25,28 @@ cgTm tm = error $ "can't cg tm: " ++ show tm
 
 cgDef :: Def () -> Doc
 cgDef (Def n r ty (Abstract Postulate) cs)
-    = parens (
-        text "define" <+> cgName n
-        <+> nestLambdas args (
+    = cgDefine n (
+        nestLambdas args (
             text "`" <> parens (
-                cgName n <+> hsep args
+                hsep . map cgName $ (n : args)
             )
         )
     )
   where
-    args = [text $ "e" ++ show i | i <- [0..nargs ty - 1]]
+    args = [UN $ "e" ++ show i | i <- [0..nargs ty - 1]]
 
-cgDef (Def n r ty (Term tm) cs)
-    = parens (
-        text "define" <+> cgName n <+> cgTm tm
-    )
+cgDef (Def n r ty (Term tm) cs) = cgDefine n $ cgTm tm
 cgDef d@(Def n r ty b cs) = error $ "can't cg def: " ++ show d
 
-nestLambdas :: [Doc] -> Doc -> Doc
-nestLambdas [] body = body
-nestLambdas (n:ns) body = parens (text "lambda" <+> parens n <+> nestLambdas ns body)
+cgDefine :: Name -> Doc -> Doc
+cgDefine n body = parens (text "define" <+> cgName n $+$ indent body)
+
+cgLambda :: Name -> Doc -> Doc
+cgLambda n body = parens (text "lambda" <+> parens (cgName n) $+$ indent body)
+
+nestLambdas :: [Name] -> Doc -> Doc
+nestLambdas [] = id
+nestLambdas (n:ns) = cgLambda n . nestLambdas ns
 
 nargs :: TT () -> Int
 nargs (Bind Pi d rhs) = 1 + nargs rhs
