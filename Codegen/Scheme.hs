200c79ed89a2f53b248debc42f248263a7ddb0c4
Matus Tejiscak <ziman@functor.sk>
2016-09-23 13:32:33 +0100

    Start working on case trees.

--- Codegen/Scheme.hs
+++ Codegen/Scheme.hs
@@ -15,34 +15,48 @@ cgTm :: TT () -> Doc
 cgTm (V n) = cgName n
 cgTm tm@(I n ty) = error $ "e-instance in codegen: " ++ show tm
 cgTm (Bind Lam (Def n () ty (Abstract Var) cs) rhs) = cgLambda n $ cgTm rhs
-cgTm (Bind Let (Def n () ty (Term tm) cs) rhs) = parens (
-        text "let" <+> parens (parens (
-            cgName n <+> cgTm tm
-        )) <+> cgTm rhs
-    )
-cgTm (App () f x) = parens (cgTm f <+> cgTm x)
+cgTm (Bind Let (Def n () ty (Term tm) cs) rhs) = cgLet [(n, cgTm tm)] (cgTm rhs)
+cgTm (App () f x) = cgApp (cgTm f) (cgTm x)
 cgTm tm = error $ "can't cg tm: " ++ show tm
+
+cgApp :: Doc -> Doc -> Doc
+cgApp f x = parens (f <+> x)
 
 cgDef :: Def () -> Doc
 cgDef (Def n r ty (Abstract Postulate) cs)
-    = cgDefine n (
-        nestLambdas args (
-            text "`" <> parens (
-                cgName n <+> hsep [text "," <> cgName n | n <- args]
-            )
+    = cgDefine n . nestLambdas args $
+        text "`" <> parens (
+            cgName n <+> hsep [text "," <> cgName n | n <- args]
         )
-    )
   where
     args = [UN $ "e" ++ show i | i <- [0..nargs ty - 1]]
 
+cgDef (Def n r ty (Patterns (CaseFun args ct)) cs) =
+    cgDefine n . nestLambdas (map defName args)
+        $ cgCase ct
+
 cgDef (Def n r ty (Term tm) cs) = cgDefine n $ cgTm tm
 cgDef d@(Def n r ty b cs) = error $ "can't cg def: " ++ show d
+
+cgCase :: CaseTree () -> Doc
+cgCase (Leaf tm) = cgTm tm
+cgCase (Case () scrut alts) =
+    cgLet [(UN "match-head", cgApp (text "car") (cgTm scrut))]
+        (text "not-implemented")
 
 cgDefine :: Name -> Doc -> Doc
 cgDefine n body = parens (text "define" <+> cgName n $+$ indent body)
 
 cgLambda :: Name -> Doc -> Doc
 cgLambda n body = parens (text "lambda" <+> parens (cgName n) $+$ indent body)
+
+cgLet :: [(Name, Doc)] -> Doc -> Doc
+cgLet defs rhs = parens (
+        text "let" <+> parens (
+            hsep [parens (cgName n <+> body) | (n, body) <- defs]
+        )
+        $+$ indent rhs
+    )
 
 nestLambdas :: [Name] -> Doc -> Doc
 nestLambdas [] = id
