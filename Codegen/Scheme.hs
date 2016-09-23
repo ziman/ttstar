4aaaa98abce315f16b6c9a84f579d9637289717b
Matus Tejiscak <ziman@functor.sk>
2016-09-23 11:54:54 +0100

    Implement cgTm.

--- Codegen/Scheme.hs
+++ Codegen/Scheme.hs
@@ -9,6 +9,17 @@ cgName :: Name -> Doc
 cgName = text . show
 
 cgTm :: TT () -> Doc
+cgTm (V n) = cgName n
+cgTm tm@(I n ty) = error $ "e-instance in codegen: " ++ show tm
+cgTm (Bind Lam (Def n () ty (Abstract Var) cs) rhs) = parens (
+        text "lambda" <+> parens (cgName n) <+> cgTm rhs
+    )
+cgTm (Bind Let (Def n () ty (Term tm) cs) rhs) = parens (
+        text "let" <+> parens (parens (
+            cgName n <+> cgTm tm
+        )) <+> cgTm rhs
+    )
+cgTm (App () f x) = parens (cgTm f <+> cgTm x)
 cgTm tm = error $ "can't cg tm: " ++ show tm
 
 cgDef :: Def () -> Doc
