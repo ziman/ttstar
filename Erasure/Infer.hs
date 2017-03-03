6e7aa656d3145a3139e5ba5a919b0c4eec8d3587
Matus Tejiscak <ziman@functor.sk>
2017-03-03 23:51:57 +0000

    Tweak inference rule.

--- Erasure/Infer.hs
+++ Erasure/Infer.hs
@@ -230,7 +230,8 @@ inferPat s pat@(PApp app_r f x) = bt ("PAT-APP", pat) $ do
         Bind Pi [Def n' pi_r ty' (Abstract Var) _noCs] retTy -> do
             tycs <- conv xty ty'
             let cs =
-                    app_r <--> pi_r   -- if we inspect here, then the pi must reflect that
+                    cond s (app_r <--> pi_r)  -- if we inspect here, then the pi must reflect that
+                                              -- but only in relevant surroundings
                     /\ app_r --> s    -- if we inspect anywhere here, then the whole pattern inspects
                     /\ fcs
                     /\ xcs
