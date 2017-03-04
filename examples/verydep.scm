eab1c5e17e951800df3fd2bed16b2d67d4d83987
Matus Tejiscak <ziman@functor.sk>
2017-03-04 00:47:23 +0000

    Port verydep.tt.

--- examples/verydep.scm
+++ examples/verydep.scm
@@ -2,15 +2,10 @@
   (letrec* (
     (Just (lambda (x)
       (list 'Just x)))
+    (Nothing (list 'Nothing))
     (Bool (list 'Bool))
     (False (list 'False))
-    (f (lambda (x)
-      (case (car x)
-        ((Just) (let* (
-          (_args-b (cdr x))
-          (b (car _args-b))
-        )
-          b)))))
+    (f (error "NOT IMPLEMENTED"))
     (main (f (Just False)))
   )
     main))
