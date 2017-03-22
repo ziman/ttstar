a575323eaa98b6c11d1800a226aa476d42914174
Matus Tejiscak <ziman@functor.sk>
2017-03-22 01:01:04 +0000

    Add forgotten foreign files.

---
+++ examples/foreign.scm
@@ -0,0 +1,14 @@
+(require-extension matchable)
+(define Type '(Type))
+(define (number->peano z s i) (if (= i 0) (list z) (list s (number->peano z s (- i 1)))))
+(define (rts-arg-peano z s i) (number->peano z s (string->number (list-ref (command-line-arguments) i))))
+(print
+  (letrec* (
+    (S (lambda (x)
+      `(S ,x)))
+    (input (rts-arg-peano 'Z 'S 0))
+    (main (match (list)
+      [()
+        (S input)]))
+  )
+    main))
