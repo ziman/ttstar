f87d24ed4ca5d2f7bef18ff63e13f117335f6464
Matus Tejiscak <ziman@functor.sk>
2017-03-23 00:56:48 +0000

    Update RTSs.

--- examples/sim-mutrec2-unerased.scm
+++ examples/sim-mutrec2-unerased.scm
@@ -2,6 +2,7 @@
 (define Type '(Type))
 (define (number->peano z s i) (if (= i 0) (list z) (list s (number->peano z s (- i 1)))))
 (define (rts-arg-peano z s i) (number->peano z s (string->number (list-ref (command-line-arguments) i))))
+(define (rts-arg-read i) (read (open-input-string (list-ref (command-line-arguments) i))))
 (print
   (letrec* (
     (Bool `(Bool))
