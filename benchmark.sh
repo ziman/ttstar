145ce45b089e04b60ce33926fec8ffa6997542bf
Matus Tejiscak <ziman@functor.sk>
2017-03-23 09:22:33 +0000

    Add more samples per input size.

--- benchmark.sh
+++ benchmark.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 PROGRAMS="palindrome"
-INPUT_SIZES="$(seq 1 3 64)"
+INPUT_SIZES="$(seq 1 1 64)"
+NSAMPLES="10"
 
 export TIMEFORMAT="%4U"
 
-echo '"program","erasure","input.size","runtime"'
+echo '"program","erasure","input.size","sample.no","runtime"'
 for prog in ${PROGRAMS}; do
     for input_size in ${INPUT_SIZES}; do
         p="examples/$prog"
-        unerased_runtime="$(
-            (time csi -qs "$p"-unerased.scm ${input_size} > /dev/null) 2>&1
-        )"
-        erased_runtime="$(
-            (time csi -qs "$p".scm ${input_size} > /dev/null) 2>&1
-        )"
-        echo "\"${prog}\",unerased,${input_size},${unerased_runtime}"
-        echo "\"${prog}\",erased,${input_size},${erased_runtime}"
+        for sample_no in $(seq ${NSAMPLES}); do
+            unerased_runtime="$(
+                (time csi -qs "$p"-unerased.scm ${input_size} > /dev/null) 2>&1
+            )"
+            erased_runtime="$(
+                (time csi -qs "$p".scm ${input_size} > /dev/null) 2>&1
+            )"
+            echo "\"${prog}\",unerased,${input_size},${sample_no},${unerased_runtime}"
+            echo "\"${prog}\",erased,${input_size},${sample_no},${erased_runtime}"
+        done
     done
 done
