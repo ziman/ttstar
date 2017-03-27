242670f801979e1881b4e034dc3032df53d2744d
Matus Tejiscak <ziman@functor.sk>
2017-03-27 03:33:55 +0100

    [WIP] add benchmark.py

---
+++ benchmark.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+import csv
+import time
+import subprocess
+import collections
+
+Input = collections.namedtuple('lo hi step')
+Program = collections.namedtuple('inputs')
+ProgramInputs = collections.namedtuple('baseline')
+
+WARMUPS = 2
+SAMPLES = 8
+
+PROGRAMS = {
+    'palindrome': Program(
+        inputs = ProgramInputs(
+            baseline = Input(lo=1, hi=64, step=1),
+        ),
+    ),
+}
+
+CSV_FIELDS = (
+    'program',
+    'inference',
+    'specialisation',
+    'verification',
+    'normalisation',
+    'compilation',
+    'input_size',
+    'sample_no',
+    'stage',  # ttstar, csc, execution
+    'runtime',
+)
+
+def time_cmd(cmd):
+    ts_start = time.perf_counter()
+    subprocess.check_call(cmd)
+    ts_end = time.perf_counter()
+
+    return ts_end - ts_start
+
+def bench_cmd(cmd):
+    for _ in range(WARMUPS):
+        time_cmd(cmd)
+
+    return [(i, time_cmd(cmd)) for i in range(SAMPLES)]
+
+def run_program(prog_name, prog):
+    result = []
+
+    for inference in (True, False):
+        for specialisation in (True, False):
+            for verification in (True, False):
+                for normalisation in (True, False):
+                    ttstar_cmd = ["./ttstar", "examples/%s.tt" % prog]
+                    if not inference:
+                        ttstar_cmd += ["--skip-inference"]
+                    if not specialisation:
+                        ttstar_cmd += ["--skip-specialisation"]
+                    if not verification:
+                        ttstar_cmd += ["--skip-verification"]
+
+                    if normalisation:
+                        ttstar_cmd += ["--dump-nf-scheme", "x.scm"]
+                    else:
+                        ttstar_cmd += ["--dump-scheme", "x.scm"]
+
+                    config = {
+                        'program': prog_name,
+                        'inference': inference,
+                        'specialisation': specialisation,
+                        'verification': verification,
+                        'normalisation': normalisation,
+                        'compilation': None,
+                        'input_size': None,
+                    }
+
+                    for sample_no, runtime in bench_cmd(ttstar_cmd):
+                        result.append({
+                            'sample_no': sample_no,
+                            'stage': 'ttstar',
+                            'runtime': runtime,
+                            **config
+                        })
+
+                    for compilation in (True, False):
+                        config['compilation'] = compilation
+
+                        if compilation:
+                            for sample_no, runtime in bench_cmd(['csc', 'x.scm']):
+                                result.append({
+                                    'sample_no': sample_no,
+                                    'stage': 'csc',
+                                    'runtime': runtime,
+                                    **config
+                                })
+
+                            exec_cmd = ["./x"]
+                        else:
+                            exec_cmd = ["csi", "-qs", "x.scm"]
+
+                        inp = prog.input.baseline
+                        for input_size in range(inp.lo, inp.hi, inp.step):
+                            config['input_size'] = input_size
+
+                            for sample_no, runtime in bench_cmd(exec_cmd):
+                                result.append({
+                                    'sample_no': sample_no,
+                                    'runtime': runtime,
+                                    'stage': 'execution',
+                                    **config
+                                })
+
+def main():
+    results = []
+
+    subprocess.check_call([
+        'cabal', 'install', '-j1'
+    ])
+
+    for prog_name, prog in PROGRAMS.items():
+        results += run_program(prog)
+
+    print(results)
+
+if __name__ == '__main__':
+    main()
