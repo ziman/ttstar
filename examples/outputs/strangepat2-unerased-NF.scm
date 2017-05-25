052e8709c594d51bac6dbe873de540bd2fa6e102
Matus Tejiscak <ziman@functor.sk>
2017-05-25 15:33:47 +0100

    Add a failing program.

---
+++ examples/outputs/strangepat2-unerased-NF.scm
@@ -0,0 +1,18 @@
+(require-extension matchable)
+(define Type '(Type))
+(define (number->peano z s i) (if (= i 0) (list z) (list s (number->peano z s (- i 1)))))
+(define (rts-arg-peano z s i) (number->peano z s (string->number (list-ref (command-line-arguments) i))))
+(define (rts-arg-read i) (read (open-input-string (list-ref (command-line-arguments) i))))
+(print
+  (letrec* (
+    (Bool `(Bool))
+    (T `(T))
+    (F `(F))
+    (f (lambda (_e0)
+      (match (list _e0)
+        (((_ ('T)))
+          F)
+        (((_ ('F)))
+          T))))
+  )
+    (f T)))
