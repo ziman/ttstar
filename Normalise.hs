986c5410803f0c6ce4b565b88d3799154bcc2c0e
Matus Tejiscak <ziman@functor.sk>
2017-06-09 10:23:51 +0100

    Make let float from the RHS of an application.

--- Normalise.hs
+++ Normalise.hs
@@ -135,6 +135,12 @@ red  NF  ctx t@(Bind b (d:ds) tm)
   where
     Bind _b ds' tm' = red NF (M.insert (defName d) d ctx) $ Bind b ds tm
 
+red form ctx t@(App r f (Bind Let ds tm))
+    | or [defName d `occursIn` f | d <- ds]
+        = error $ "app+let reduction: capture avoidance not implemented yet"
+    | otherwise
+        = red form ctx $ Bind Let ds (App r f tm)
+
 red form ctx t@(App r (Bind Let ds tm) x)
     | or [defName d `occursIn` x | d <- ds]
         = error $ "app+let reduction: capture avoidance not implemented yet"
