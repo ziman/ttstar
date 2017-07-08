c7403f0ad93c59418986c4497393e85159cff35f
Matus Tejiscak <ziman@functor.sk>
2017-07-08 21:09:19 +0200

    Prune constant clauses to terms.

--- Erasure/Prune.hs
+++ Erasure/Prune.hs
@@ -16,7 +16,10 @@ pruneDef (Def n R ty body mcs)
 pruneBody :: Body Relevance -> Body ()
 pruneBody (Abstract a) = Abstract a
 pruneBody (Term tm)    = Term $ pruneTm tm
-pruneBody (Clauses cs) = Clauses $ map pruneClause cs
+pruneBody (Clauses cs) = 
+    case map pruneClause cs of
+        [Clause [] [] rhs] -> Term rhs
+        cs' -> Clauses cs'
 
 pruneClause :: Clause Relevance -> Clause ()
 pruneClause (Clause pvs lhs rhs)
