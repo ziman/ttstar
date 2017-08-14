19d5785506e996b518953b65230056ac72479819
Matus Tejiscak <ziman@functor.sk>
2017-08-14 13:42:51 +0100

    Implement var rule.

--- Codegen/ToIR.hs
+++ Codegen/ToIR.hs
@@ -6,8 +6,6 @@ import TT.Pretty ()
 
 import Codegen.IR
 
-import Data.List
-
 toIR :: TT () -> IR
 toIR = irTm 0
 
@@ -36,6 +34,66 @@ argNames (Bind Pi ds rhs) = map defName ds ++ argNames rhs
 argNames _ = []
 
 compile :: Int -> [Clause ()] -> ICaseTree
-compile pv clauses = error "not implemented"
+compile pv clauses
+    = matchSort pvars pats $ ILeaf (IError "unreachable case")
   where
-    columns = transpose [snd $ unApplyPat lhs | Clause pvs lhs rhs <- clauses]
+    pv' = pv + width
+    pvars = [pv + i | i <- [0..width-1]]
+    widths = map (length . fst) pats
+    width = if maximum widths == minimum widths then minimum widths else error "compile: pat width mismatch"
+    pats = [(map snd . snd $ unApplyPat lhs, irTm pv' rhs) | Clause pvs lhs rhs <- clauses]
+
+matchSort :: [Int] -> [([Pat ()], IR)] -> ICaseTree -> ICaseTree
+matchSort vars pats err = match vars pats err
+
+match :: [Int] -> [([Pat ()], IR)] -> ICaseTree -> ICaseTree
+match [] [([], rhs)] err = ILeaf rhs
+match vars pats err
+    | isVar firstPat
+    = let (pats', rest) = span (isVar . head . fst) pats
+        in ruleVar vars pats' $ match vars rest err
+
+    | isCtor firstPat
+    = let (pats', rest) = span (isCtor . head . fst) pats
+        in ruleCtor vars pats' $ match vars rest err
+
+    | otherwise
+    = error $ "match: unsupported pattern: " ++ show firstPat
+  where
+    firstPat = head . fst . head $ pats
+
+    isVar (PV n) = True
+    isVar _ = False
+
+    isCtor pat@(PApp r f x) | (PV cn, args) <- unApplyPat pat = True
+    isCtor _ = False
+
+ruleVar :: [Int] -> [([Pat ()], IR)] -> ICaseTree -> ICaseTree
+ruleVar (v:vs) pats err = matchSort vs pats' err
+  where
+    pats' = [(ps, substIR (irName n) v rhs) | (PV n : ps, rhs) <- pats]
+
+ruleCtor :: [Int] -> [([Pat ()], IR)] -> ICaseTree -> ICaseTree
+ruleCtor vars pats err = undefined
+
+-- we know that the substituted name is unique
+substIR :: IName -> Int -> IR -> IR
+substIR n v (IV n') | n == n' = IV $ IPV v
+substIR n v iv@(IV n') = iv
+substIR n v (ILam n' rhs) = ILam n' (substIR n v rhs) 
+substIR n v (ILet n' b rhs) = ILet n' (substIRB n v b) (substIR n v rhs)
+substIR n v (IApp f x) = IApp (substIR n v f) (substIR n v x)
+substIR n v ir@(IError s) = ir
+
+substIRB :: IName -> Int -> IBody -> IBody
+substIRB n v (ICaseTree ct) = ICaseTree $ substIRCT n v ct
+substIRB n v b = b
+
+substIRCT :: IName -> Int -> ICaseTree -> ICaseTree
+substIRCT n v (ILeaf tm) = ILeaf $ substIR n v tm
+substIRCT n v (ICase n' alts)
+    = ICase (if n == n' then IPV v else n') (map (substIRA n v) alts)
+
+substIRA :: IName -> Int -> IAlt -> IAlt
+substIRA n v (ICtor cn pvs rhs) = ICtor cn pvs $ substIRCT n v rhs
+substIRA n v (IDefault rhs) = IDefault $ substIRCT n v rhs
