d0430595481efa650a3b9d44beab4c8b8a8fd648
Matus Tejiscak <ziman@functor.sk>
2017-08-14 17:07:52 +0100

    Tweak pretty-printing.

--- Codegen/Pretty.hs
+++ Codegen/Pretty.hs
@@ -18,27 +18,32 @@ instance Pretty IR where
     pretty (ILet n body rhs)
         = text "let" <+> pretty n <+> text "=" <+> pretty body
             $$ indent (text "in" <+> pretty rhs)
-    pretty (IApp f x) = parens (pretty f) <+> parens (pretty x)
+    pretty (IApp f x) = parens (pretty f <+> pretty x)
     pretty (IError s) = text "error" <+> text (show s)
 
 instance Pretty IBody where
     pretty (IForeign code) = text "foreign" <+> text (show code)
     pretty (IConstructor arity) = text "constructor" <+> text (show arity)
+    pretty (ICaseFun [] (ILeaf tm)) = pretty tm
     pretty (ICaseFun pvs ct)
-        = hsep [text "\\pv" <> int v <> text "." | v <- pvs]
+        = hsep [text "\\" <> pv v <> text "." | v <- pvs]
             $$ indent (pretty ct)
 
 instance Pretty ICaseTree where
     pretty (ILeaf tm) = pretty tm
     pretty (ICase v alts)
-        = text "case" <+> text "pv" <> int v <+> text "of"
+        = text "case" <+> pv v <+> text "of"
             $$ indent (vcat . map pretty $ alts)
 
 instance Pretty IAlt where
     pretty (ICtor cn pvs ct)
-        = pretty cn <+> hsep [text "pv" <> int v | v <- pvs]
+        = pretty cn <+> hsep [pv v | v <- pvs]
             <+> text "=>" <+> pretty ct
     pretty (IDefault ct)
         = text "_" <+> text "=>" <+> pretty ct
 
+pv :: Int -> Doc
+pv i = text "_pv" <> int i
+
+indent :: Doc -> Doc
 indent = nest 2
