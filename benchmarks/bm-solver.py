4f4e04b2a5f273cfc7d0c83174af36721ff44cb1
Matus Tejiscak <ziman@functor.sk>
2017-08-17 16:52:42 +0100

    Add solver benchmarks.

---
+++ benchmarks/bm-solver.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import json
+import subprocess
+
+WARMUPS = 4
+SAMPLES = 32
+
+def time_cmd(cmd):
+    # TODO: use perf stat -x \;
+    ts_start = time.perf_counter()
+    subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+    ts_end = time.perf_counter()
+
+    return ts_end - ts_start
+
+def bench_cmd(cmd):
+    try:
+
+        for _ in range(WARMUPS):
+            time_cmd(cmd)
+
+        for i in range(SAMPLES):
+            yield (i, time_cmd(cmd))
+
+    except subprocess.CalledProcessError:
+        yield (0, None)
+
+def main():
+    print('"program","iteration","erasure","annotations","evars","annotations_used","duration"')
+    for fname in os.listdir('../examples'):
+        cmd = ['../ttstar', '--dump-pretty', '/dev/null', '--dump-stats', '/tmp/stats.json']
+
+        # with erasure
+        for i, t in bench_cmd(cmd + ["../examples/" + fname]):
+            if t is None:  # some error
+                continue
+
+            with open('/tmp/stats.json') as f:
+                doc = json.load(f)
+
+            print('"%s","%d","%s","%d","%d","%d","%f"' % (
+                fname.split('.')[0], i, True,
+                doc['annotations'], doc['evars'], doc['annotations_used'],
+                t,
+            ))
+
+if __name__ == '__main__':
+    main()
