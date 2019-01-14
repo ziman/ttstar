83ee4d2a902525ee7ef6a4fc40a14615733a9317
Matus Tejiscak <ziman@functor.sk>
2019-01-14 21:15:04 +0100

    Add stats collector.

---
+++ benchmarks/stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+programs="$(ls ../examples/ |  grep '.tt$' | cut -d. -f1)"
+
+echo 'program,loc,bytes,evars,annotations,annotations_used'
+for p in $programs; do
+    fname=../examples/${p}.tt
+    loc=$(wc -l < $fname)
+    bytes=$(wc -c < $fname)
+    evars=$(ttstar $fname --dump-stats /dev/stdout 2>/dev/null | jq .evars)
+    annotations=$(ttstar $fname --dump-stats /dev/stdout 2>/dev/null | jq .annotations)
+    annotations_used=$(ttstar $fname --dump-stats /dev/stdout 2>/dev/null | jq .annotations_used)
+    echo $p,$loc,$bytes,${evars:-0},${annotations:-0},${annotations_used:-0}
+done
